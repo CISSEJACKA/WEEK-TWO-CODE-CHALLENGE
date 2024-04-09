94b8560d61f5d12994d2a46dda759cd2547fd397
JACKA <jackarcisse@gmail.com>
2024-04-09 16:09:23 +0300

    complete lab

--- question_2/generateArray.js
+++ question_2/generateArray.js
@@ -1,12 +1,23 @@
+// Function to generate an array of consecutive numbers between two given numbers
 function generateArray(num1, num2) {
-    const start = Math.min(num1, num2);
-    const end = Math.max(num1, num2);
+
+    // Determine the start and end numbers for the array
+    const start = Math.min(num1, num2); 
+    const end = Math.max(num1, num2);   
+    
+    // Initialize an empty array to store the generated numbers
     const array = [];
+    
+    // Loop from the start to the end number 
     for (let i = start; i <= end; i++) {
-        array.push(i);
+  // Push the current number to the array
+        array.push(i); 
     }
+    
+    // Return the generated array
     return array;
 }
 
-console.log(generateArray(4, 7)); 
-console.log(generateArray(-4, 7));
+// Test the generated Array function 
+console.log(generateArray(4, 7));   
+console.log(generateArray(-4, 7));  
